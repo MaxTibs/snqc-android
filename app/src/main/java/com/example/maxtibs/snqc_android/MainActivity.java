b2dde69838f31f84f03cfca95a6cb3be96bd276f
Maxime Thibault <maxx_thibault@hotmail.com>
2018-12-18 20:44:03 -0500

    BottomNavigation fix

--- app/src/main/java/com/example/maxtibs/snqc_android/MainActivity.java
+++ app/src/main/java/com/example/maxtibs/snqc_android/MainActivity.java
@@ -19,13 +19,13 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
-                case R.id.navigation_home:
+                case R.id.navigation_dashboard:
                     getSupportFragmentManager().beginTransaction().replace(R.id.navigation_frame, new DashboardFragment()).commit();
                     return true;
-                case R.id.navigation_dashboard:
+                case R.id.navigation_video:
                     getSupportFragmentManager().beginTransaction().replace(R.id.navigation_frame, new VideoFragment()).commit();
                     return true;
-                case R.id.navigation_notifications:
+                case R.id.navigation_toolkits:
                     getSupportFragmentManager().beginTransaction().replace(R.id.navigation_frame, new ToolkitFragment()).commit();
                     return true;
                 case R.id.navigation_test:
@@ -43,8 +43,8 @@ public class MainActivity extends AppCompatActivity {
 
         //mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setSelectedItemId(R.id.navigation_dashboard);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        navigation.setSelectedItemId(R.id.navigation_dashboard);
     }
 
 }

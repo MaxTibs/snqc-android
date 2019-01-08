5202a4bd946156b7bf5e0fdd458af7422512389c
Maxime Thibault <maxx_thibault@hotmail.com>
2019-01-08 07:05:57 -0500

    Snooze button ok

--- app/src/main/java/com/example/maxtibs/snqc_android/toolkit/tools/SleepMode/SleepModeNotification.java
+++ app/src/main/java/com/example/maxtibs/snqc_android/toolkit/tools/SleepMode/SleepModeNotification.java
@@ -1,23 +1,30 @@
 package com.example.maxtibs.snqc_android.toolkit.tools.SleepMode;
 
 import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.support.v4.app.NotificationManagerCompat;
 
 import com.example.maxtibs.snqc_android.R;
 import com.example.maxtibs.snqc_android.utilities.Notification;
 
-public class SleepModeNotification {
+public class SleepModeNotification extends BroadcastReceiver{
+
+    private static final String SNOOZE = "SleepModeNotification.snooze";
+    private static final String LOCK = "SleepModeNotification.lock";
+
+    private static final String CHANID = "0";
 
     public static void notify(Context context) {
         //Send notification to user
-        Notification notification = new Notification(context, "0", "Chan", "desc");
+        Notification notification = new Notification(context, CHANID, "Chan", "desc");
         notification.setDefaultNotification(context, "SNQC", "Fermer l'écran");
 
-        Intent snooze = new Intent(context, SleepModeLifecycle.class);
-        snooze.setAction("snooze");
-        Intent lockScreen = new Intent(context, SleepModeLifecycle.class);
-        lockScreen.setAction("lock_screen");
+        Intent snooze = new Intent(context, SleepModeNotification.class);
+        snooze.setAction(SNOOZE);
+        Intent lockScreen = new Intent(context, SleepModeNotification.class);
+        lockScreen.setAction(LOCK);
 
         PendingIntent snoozePending = PendingIntent.getBroadcast(context, 0, snooze, 0);
         PendingIntent lockScreenPending = PendingIntent.getBroadcast(context, 0, lockScreen, 0);
@@ -33,4 +40,18 @@ public class SleepModeNotification {
         notification.push(context);
     }
 
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+        String action = intent.getAction();
+
+        switch (action) {
+            case(SNOOZE):
+                NotificationManagerCompat notificationManager = NotificationManagerCompat.from(context);
+                notificationManager.cancel(Integer.valueOf(SleepModeNotification.CHANID)); //Dismiss notification
+                break;
+            case (LOCK):
+                break;
+        }
+    }
 }

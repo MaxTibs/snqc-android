e2e16893b3a55be5854c222d4bc66aba28d1e7df
Samuel <samuelthibault99@hotmail.com>
2019-01-08 20:57:22 -0500

    first commit

--- app/src/main/java/com/example/maxtibs/snqc_android/VideoFragment.java
+++ app/src/main/java/com/example/maxtibs/snqc_android/VideoFragment.java
@@ -5,17 +5,48 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.TextView;
 
+import com.example.maxtibs.snqc_android.toolkit.Tools.Videos.Video;
+import com.example.maxtibs.snqc_android.toolkit.Tools.Videos.VideoAdapter;
+
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
 public class VideoFragment extends Fragment {
+
+    private HashMap<String, String> URLs = new HashMap<String, String>();
 
     public VideoFragment() {}
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        TextView tv = new TextView(getActivity());
-        tv.setText("VIDEO FRAGMENT");
-        return tv;
+        // Defines URL
+        URLs.put("http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4", "Big Bunny");
+        URLs.put("http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4", "Elephant Dream");
+        URLs.put("http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4", "For Bigger Blazes");
+
+        //Defines every video
+        ArrayList<Video> videoArrayList = new ArrayList<>();
+        Iterator it = URLs.entrySet().iterator();
+        while (it.hasNext()) {
+            Map.Entry pair = (Map.Entry)it.next();
+            videoArrayList.add(new Video((String)pair.getKey(), (String)pair.getValue()));
+        }
+
+        //Create adapter
+        VideoAdapter videoAdapter = new VideoAdapter(getContext(), videoArrayList);
+
+        //Set video adapter to listview
+        View videoList = getLayoutInflater().inflate(R.layout.video_list, null);
+        ListView lv = videoList.findViewById(R.id.videoListView);
+        lv.setAdapter(videoAdapter);
+
+        return lv;
     }
 }

9fbdd612828159a116da16689eb2ffaf6498c7f5
Jeremy <dube.jeremy96@gmail.com>
2019-01-22 22:48:51 -0500

    Get all bitmap thumbnails at start of the app - without loading

--- app/src/main/java/com/example/maxtibs/snqc_android/MainActivity.java
+++ app/src/main/java/com/example/maxtibs/snqc_android/MainActivity.java
@@ -6,8 +6,15 @@ import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 
+import com.example.maxtibs.snqc_android.Videos.Video;
+import com.example.maxtibs.snqc_android.utilities.VideoUtility;
+
+import java.util.ArrayList;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private ArrayList<Video> videoArrayList = new ArrayList<>();
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -19,7 +26,11 @@ public class MainActivity extends AppCompatActivity {
                     getSupportFragmentManager().beginTransaction().replace(R.id.navigation_frame, new DashboardFragment()).commit();
                     return true;
                 case R.id.navigation_video:
-                    getSupportFragmentManager().beginTransaction().replace(R.id.navigation_frame, new VideoFragment()).commit();
+                    Bundle bundle = new Bundle();
+                    bundle.putParcelableArrayList("videoList", videoArrayList);
+                    VideoFragment videoFragment = new VideoFragment();
+                    videoFragment.setArguments(bundle);
+                    getSupportFragmentManager().beginTransaction().replace(R.id.navigation_frame, videoFragment).commit();
                     return true;
                 case R.id.navigation_toolkits:
                     getSupportFragmentManager().beginTransaction().replace(R.id.navigation_frame, new ToolkitFragment()).commit();
@@ -41,6 +52,26 @@ public class MainActivity extends AppCompatActivity {
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
         navigation.setSelectedItemId(R.id.navigation_dashboard);
+
+        // Define every video
+        videoArrayList.add(new Video("http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4", "Big Bunny"));
+        videoArrayList.add(new Video("http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4", "Elephant Dream"));
+        videoArrayList.add(new Video("http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4", "For Bigger Blaze"));
+        videoArrayList.add(new Video("http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerEscapes.mp4", "For Bigger Escape"));
+        videoArrayList.add(new Video("http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerFun.mp4", "For Bigger Fun"));
+        videoArrayList.add(new Video("http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerJoyrides.mp4", "For Bigger Joyrides"));
+        videoArrayList.add(new Video("http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerMeltdowns.mp4", "For Bigger Meltdowns"));
+        videoArrayList.add(new Video("http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/Sintel.mp4", "Sintel"));
+        videoArrayList.add(new Video("http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/SubaruOutbackOnStreetAndDirt.mp4", "Subaru Outback On Street And Dirt"));
+        videoArrayList.add(new Video("http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/TearsOfSteel.mp4", "Tears of Steel"));
+        videoArrayList.add(new Video("http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/VolkswagenGTIReview.mp4", "Volkswagen GTI Review"));
+        videoArrayList.add(new Video("http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/WeAreGoingOnBullrun.mp4", "We Are Going On Bullrun"));
+        videoArrayList.add(new Video("http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/WhatCarCanYouGetForAGrand.mp4", "What care can you get for a grand?"));
+
+        for (Video video : videoArrayList) {
+            video.imageName = video.title.replaceAll(" ", "_");
+            VideoUtility.saveToCahche(getApplicationContext(), video, VideoUtility.retrieveVideoFrameFromVideo(video.url));
+        }
     }
 
 }
--- app/src/main/java/com/example/maxtibs/snqc_android/Videos/Video.java
+++ app/src/main/java/com/example/maxtibs/snqc_android/Videos/Video.java
@@ -6,6 +6,8 @@ import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
 import android.media.MediaMetadataRetriever;
 import android.os.Build;
+import android.os.Parcel;
+import android.os.Parcelable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,7 +19,17 @@ import com.example.maxtibs.snqc_android.R;
 import java.util.HashMap;
 
 
-public class Video {
+public class Video implements Parcelable {
+
+    public static final Parcelable.Creator CREATOR = new Parcelable.Creator() {
+        public Video createFromParcel(Parcel in) {
+            return new Video(in);
+        }
+
+        public Video[] newArray(int size) {
+            return new Video[size];
+        }
+    };
 
     public String url;
     public String title;
@@ -26,6 +38,7 @@ public class Video {
     public Video(String url, String title) {
         this.url = url;
         this.title = title;
+        this.imageName = "";
     }
 
     public void start(Context context) {
@@ -33,4 +46,32 @@ public class Video {
         intent.putExtra("URL", this.url);
         context.startActivity(intent);
     }
+    // Parcelling part
+    public Video(Parcel in){
+        this.url = in.readString();
+        this.title = in.readString();
+        this.imageName =  in.readString();
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(this.url);
+        dest.writeString(this.title);
+        dest.writeString(this.imageName);
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "url='" + url + '\'' +
+                ", title='" + title + '\'' +
+                ", imageName='" + imageName + '\'' +
+                '}';
+    }
+
 }
